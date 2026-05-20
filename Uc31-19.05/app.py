cf50706f839b338ac429729c246b2f04d4714aa3
Bekizxn24 <Lorenrebecca7@gmail.com>
2026-05-20 17:14:57 -0300

    Add files via upload

---
+++ Uc31-19.05/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/Inicio')
+def FunçãoInicio():
+    return render_template('index.html')
+
+@app.route('/Cardapio')
+def FunçãoCardapio():
+    return render_template('cardapio.html')
+
+@app.route('/Cliente')
+def FunçãoCliente():
+    return render_template('cliente.html')
+
+@app.route('/Contato')
+def FunçãoContato():
+    return render_template('contato.html')
+
+@app.route('/Lanche')
+def FunçãoLanche():
+    return render_template('lanche.html')
+
+@app.route('/Pedidos')
+def FunçãoPedidos():
+    return render_template('pedidos.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
