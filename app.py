dce3b304d935b750704de22f88ddacc4f39b5478
Bekizxn24 <Lorenrebecca7@gmail.com>
2026-05-27 15:35:27 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-if __name__ == '__main__':
+@app.route("/cursos")
+def cursos():
+    return render_template("cursos.html")
+
+@app.route("/professores")
+def professores():
+    return render_template("professores.html")
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
